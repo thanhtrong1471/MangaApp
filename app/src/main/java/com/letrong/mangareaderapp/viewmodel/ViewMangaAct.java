aa25f909acb6430fa10364e04d6b7d0f8c7ef26b
LVTTrong <thanhtrong.25021998@gmail.com>
2024-01-12 14:24:51 +0700

    full

---
+++ app/src/main/java/com/letrong/mangareaderapp/viewmodel/ViewMangaAct.java
@@ -0,0 +1,66 @@
+package com.letrong.mangareaderapp.viewmodel;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.letrong.mangareaderapp.R;
+import com.letrong.mangareaderapp.adapter.MyViewPagerAdapter;
+import com.letrong.mangareaderapp.model.Chapter;
+import com.letrong.mangareaderapp.util.BookFlipPageTransformer;
+
+public class ViewMangaAct extends AppCompatActivity implements View.OnClickListener {
+
+    ViewPager viewPager;
+    TextView txtChapterName;
+    View btnBack;
+    Chapter chapter;
+    SeekBar seekBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_manga);
+        chapter =(Chapter) getIntent().getSerializableExtra("chapter");
+        initUI();
+    }
+    private  void initUI(){
+        viewPager = findViewById(R.id.viewPager);
+        txtChapterName = findViewById(R.id.txtCurrentChapter);
+        btnBack = findViewById(R.id.chapter_back);
+        txtChapterName.setText(chapter.getName());
+        seekBar = findViewById(R.id.SeekbarManga);
+
+        fetchhLinks(chapter);
+
+
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.chapter_back:
+                Toast.makeText(this, "BACK", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+        }
+    }
+    private void fetchhLinks(Chapter chap){
+        if(chap.getLinks() != null){
+            if(chap.getLinks().size() > 0){
+                MyViewPagerAdapter adapter = new MyViewPagerAdapter(getBaseContext(), chap.getLinks());
+                viewPager.setAdapter(adapter);
+                BookFlipPageTransformer bookFlipPageTransformer = new BookFlipPageTransformer();
+                bookFlipPageTransformer.setScaleAmountPercent(10f);
+                viewPager.setPageTransformer(true, bookFlipPageTransformer);
+                
+            }
+        }
+    }
+}
